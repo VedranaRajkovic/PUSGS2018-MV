2b8c41cd2ab76509f9789e53a096bb5794294354
VedranaRajkovic <r_vedrana@yahoo.com>
2018-06-08 09:33:33 +0200

    Login

--- angular/hello/src/app/app.module.ts
+++ angular/hello/src/app/app.module.ts
@@ -1,17 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpModule} from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientXsrfModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main/main.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
+const Routes=[
+  {
+    path: '',
+    redirectTo: "/home",
+    pathMatch:'full'
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  }
+  ]
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MainComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
