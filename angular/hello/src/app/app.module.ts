5e553afef69888981bf254e32041a346fb9004ff
e318-2014 <e318-2014@P04-09>
2018-06-08 10:19:14 +0200

    Pocetna stranica bootstrap

--- angular/hello/src/app/app.module.ts
+++ angular/hello/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main/main.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MainComponent
+    MainComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
